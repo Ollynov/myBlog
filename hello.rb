c0b5251084f7e726a6d53ef008fa7769970468a7
Aljosha Novakovic <ollynov@gmail.com>
2017-08-19 21:11:17 +0000

    added a gift

--- hello.rb
+++ hello.rb
@@ -1,1 +1,11 @@
-puts 'hello World!'
+ gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+          'yoshs gift'
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
